3d80f996bcbed7a4c47ddc7096c7d2d045dd40a0
BluePsion <farnorthgreen@gmail.com>
2017-01-20 23:42:59 -0500

    Curses and File Structure
    Set up file structure for project and added curses capability.

---
+++ command-pattern/curseroguewindow.h
@@ -0,0 +1,9 @@
+#ifndef CURSEROGUEWINDOW_H_INCLUDED
+#define CURSEROGUEWINDOW_H_INCLUDED
+
+void openRogueWindow();
+void closeRogueWindow();
+void drawABoxAroundEdge(char wallChar = '#');
+
+
+#endif // CURSEROGUEWINDOW_H_INCLUDED
---
+++ command-pattern/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "curseroguewindow.h"
+using namespace std;
+
+int main()
+{
+    openRogueWindow();
+    closeRogueWindow();
+    return 0;
+}
